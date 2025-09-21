04a32001e0a5327166d55b1403a7560a73d9bae8
Santiagoacevedo5 <santiago.acevedoc1@udea.edu.co>
2025-09-21 12:59:40 -0500

    no me deja hacer testearlo

--- SistemaDeMonitoreo/program.cs
+++ SistemaDeMonitoreo/program.cs
@@ -1,15 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SistemaDeMonitoreo
+class Program
 {
-    internal class SistemaDeMonitoreo
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        { 
-           
+        TemperatureSensor sensor = null;
+        alertSystem alert = new alertSystem();
+        bool activo = true;
+
+        while (activo)
+        {
+            Console.Clear();
+            Console.WriteLine("=== MENÚ DE MONITOREO DE TEMPERATURA ===");
+            Console.WriteLine("1. Iniciar sensor");
+            Console.WriteLine("2. Salir");
+            Console.Write("Seleccione una opción: ");
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    if (sensor == null)
+                    {
+                        sensor = new TemperatureSensor();
+
+                        sensor.HighTemperatureReached += (sender, e) =>
+                        {
+                            var args = new TemperatureEventArgs(((TemperatureSensor)sender).GetCurrentTemperature());
+                            alert.OnHighTemperature(sender, args);
+                        };
+
+                        sensor.LowTemperatureReached += (sender, e) =>
+                        {
+                            var args = new TemperatureEventArgs(((TemperatureSensor)sender).GetCurrentTemperature());
+                            alert.OnLowTemperature(sender, args);
+                        };
+
+                        Console.WriteLine("Sensor iniciado. Presiona Enter para continuar...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("El sensor ya está en funcionamiento.");
+                    }
+                    Console.ReadLine();
+                    break;
+
+                case "2":
+                    activo = false;
+                    Console.WriteLine("Saliendo del sistema...");
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    Console.ReadLine();
+                    break;
+            }
+        }
     }
 }
